e1824b09a117dcbd6318dbc08145f15b1df12f54
Marcos Mantovani <mantovani.marcos@gmail.com>
2023-11-17 23:57:03 -0300

    First structure.

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:riverpod_calculator/colors.dart';
 
-void main() {
+Future main() async {
+  // Ensure flutter created the the binding with host platform
+  WidgetsFlutterBinding.ensureInitialized();
+  // Lock the screen orientation to portrait. Only work with mobile devices.
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
   runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
+  static const String title = 'Calculator';
+
   const MainApp({super.key});
 
   @override
+  Widget build(BuildContext context) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: title,
+        theme: ThemeData(
+          scaffoldBackgroundColor: MyColors.background1,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: const MainPage(title: title),
+      );
+}
+
+class MainPage extends StatefulWidget {
+  final String title;
+
+  const MainPage({super.key, required this.title});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return Material(
+      color: Colors.red,
+      child: Container(
+        alignment: Alignment.center,
+        child: Text(
+          'Hello',
+          style: Theme.of(context).textTheme.displayLarge,
         ),
       ),
     );
